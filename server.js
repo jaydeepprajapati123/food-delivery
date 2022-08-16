426d22b56b56fda64e8985458f3ec2f4574095ff
Jaydeep Prajapati <prajapatijaydeep900@gmail.com>
2022-08-16 14:35:34 +0530

    initial commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import router from "./routes";
 import { ErrorHandler } from "./middlewares";
 
 import mongoose from "mongoose";
-mongoose.connect(DB_URL).then(()=> console.log("database connected....")).catch((error)=> console.log('err:- ',error));
+mongoose.connect("mongodb+srv://jaydeep:jaydeep1153@cluster0.9l1hv.mongodb.net/food_delivery?retryWrites=true&w=majority").then(()=> console.log("database connected....")).catch((error)=> console.log('err:- ',error));
 
 const app = express();
 
